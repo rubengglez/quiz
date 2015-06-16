4750c249fb64db69771d3f6d0ce3551666ace124
Rubén García GLEz <rubengglez@gmail.com>
2015-06-16 18:17:56 +0200

    Crear preguntas y validadción de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -17,20 +17,20 @@ exports.index = function(req, res){
 	var search = req.query.search;
 	if ( search === undefined ) {
 		models.Quiz.findAll().then(function(quizes){
-			res.render('quizes/index', {quizes: quizes});
+			res.render('quizes/index', {quizes: quizes, errors: []});
 		});
 
 	}else{ // GET buscador de preguntas
 		models.Quiz.findAll({where: ["UPPER(pregunta) like ?", '%' + search.toUpperCase().replace(/\s+/g, '%') + '%']})
 		.then(function(quizes){
-			res.render('quizes/index', {quizes:quizes});
+			res.render('quizes/index', {quizes:quizes, errors: []});
 		});
 	}
 };
 
 // GET /quizes/:id
 exports.show = function(req, res){
-	res.render('quizes/show', {quiz: req.quiz });
+	res.render('quizes/show', {quiz: req.quiz, errors: [] });
 };
 
 // GET /quizes/:id/answer
@@ -40,6 +40,34 @@ exports.answer = function(req, res){
 		if ( req.query.respuesta === req.quiz.respuesta ){
 			resultado = 'Correcto';
 		}
-		res.render('quizes/answer', {quiz: req.quiz, respuesta : resultado});
+		res.render('quizes/answer', {quiz: req.quiz, respuesta : resultado, errors: []});
 	});
+};
+
+// GET /quizes/new
+exports.new = function(req, res){
+	var quiz = models.Quiz.build( // crea objeto quiz
+		{pregunta: "pregunta", respuesta: "Respuesta"}
+	);
+	res.render('quizes/new', {quiz: quiz, errors: []});
+};
+
+// POST /quizes/create
+exports.create = function(req, res){
+	var quiz = models.Quiz.build( req.body.quiz );
+
+	quiz
+	.validate()
+	.then(
+		function(err){
+			if (err) {
+				res.render('quizes/new', {quiz: quiz, errors: err.errors});
+
+			} else {
+				// save: guarda en DB campos pregunta y respuesta de quiz
+				quiz.save({fields: ["pregunta", "respuesta"]})
+				.then( function(){ res.redirect('/quizes')});
+			}   // res.redirect: Redirección HTTp (URL relativo) lista de preguntas
+		}
+	);
 };
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: [] });
 });
 
 router.param('quizId', quizController.load); // autoload :quizId
@@ -15,8 +15,11 @@ router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
+
 router.get('/author', function(req, res){
-	res.render('author');
+	res.render('author', {errors: []});
 });
 
 module.exports = router;
